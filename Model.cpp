c32c0dc0cf10b01dbec9caeb99d490d0c88c0d2a
kdjlyy <kdjlyy@qq.com>
2021-03-01 19:59:26 +0800

    1

---
+++ Model.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define ios ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define LL long long
+#define ULL unsigned long long
+const int inf = 0x3f3f3f3f;
+const int maxn=1e5+10;
+
+using namespace std;
+
+
+void PRINT(vector<int>& v) {
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    ios
+/////////////////////
+
+    return 0;
+}
